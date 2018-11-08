6e217bfa3a7f92eb3be4f0c98b5cabb8591d441c
Adrianna Vu <advu@live.unc.edu>
2018-11-08 12:54:04 -0500

    created data table

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,61 @@
 console.log('scripts loaded');
 
-var analysis = document.getElementById('analysis');
+//CREATE ANALYSIS TOOLTIP
+var analysisLabel = document.getElementById('analysisLabel');
 var analysisTooltip = document.getElementById('analysisTooltip');
 
-analysis.addEventListener('mouseenter', function() {
-  console.log('Analysis hovered over');
-  analysisTooltip.style.opacity = '1';
+analysisLabel.addEventListener('mouseenter', function() {
+  console.log('analysisLabel hovered over');
+  analysisTooltip.style.visibility = 'visible';
   var x = event.screenX; // Get the horizontal coordinate
   var y = event.screenY; // Get the vertical coorindate
   console.log(x);
   console.log(y);
   analysisTooltip.style.top = (y - 110) + 'px';
-  analysisTooltip.style.left = (x - 80) + 'px';
+  analysisTooltip.style.left = (x - 60) + 'px';
 
 });
 
-analysis.addEventListener('mouseleave', function() {
-  console.log('Analysis hovered over');
-  analysisTooltip.style.opacity = '0';
+analysisLabel.addEventListener('mouseleave', function() {
+  console.log('analysisLabelhovered over');
+  analysisTooltip.style.visibility = 'hidden';
 });
+
+//EXECUTIVE PERCENTAGES BY RACE/ETHNICITY IN PRIVATE INDUSTRY 2008-2016 DATA TABLE
+$('#execDataTable').DataTable({
+  ajax: {
+    url: '/js/EEO-1.json',
+    dataSrc: ''
+  },
+  columns: [{
+      "data": "Executives Race/Ethnicity"
+    },
+    {
+      "data": "2008"
+    },
+    {
+      "data": "2009"
+    },
+    {
+      "data": "2010"
+    },
+    {
+      "data": "2011"
+    },
+    {
+      "data": "2012"
+    },
+    {
+      "data": "2013"
+    },
+    {
+      "data": "2014"
+    },
+    {
+      "data": "2015"
+    },
+    {
+      "data": "2016"
+    }
+  ]
+}); //close DataTables
